869264cb995e9eb6a6ce8dcb6aeb5f1b9c2f045e
ashytrashbean <annalu.jansson@gmail.com>
2026-09-22 14:08:48 +0200

    Enhance FilterSelect component styling for better layout and responsiveness

--- src/components/Filtering/FilterSelect.jsx
+++ src/components/Filtering/FilterSelect.jsx
@@ -2,7 +2,7 @@
 
 export default function FilterSelect({label, value, options, optionValue, optionLabel, onChange}){
     return(
-        <label> {label}: <select value={value} onChange={onChange}>
+        <label className="filterSelect"> {label}: <select value={value} onChange={onChange}>
                 <option value={""} hidden>Choose {label}</option>
                 {options.map((opt)=> <option key={opt[optionValue]} value={opt[optionValue]}>{opt[optionLabel]}</option>)}
             </select>
