d54c40f6d37eaaaa6b334dc035a3c5a27b98dd5e
ashytrashbean <annalu.jansson@gmail.com>
2026-09-22 12:26:40 +0200

    Add hidden attribute to default option in FilterSelect for better accessibility

--- src/components/Filtering/FilterSelect.jsx
+++ src/components/Filtering/FilterSelect.jsx
@@ -3,7 +3,7 @@
 export default function FilterSelect({label, value, options, optionValue, optionLabel, onChange}){
     return(
         <label> {label}: <select value={value} onChange={onChange}>
-                <option value={""}>Choose {label}</option>
+                <option value={""} hidden>Choose {label}</option>
                 {options.map((opt)=> <option key={opt[optionValue]} value={opt[optionValue]}>{opt[optionLabel]}</option>)}
             </select>
         </label>
